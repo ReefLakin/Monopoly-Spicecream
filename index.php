c29c241c2b93bbdb0d39f598f6a9a15a77171761
ReefLakin <reeflakin2@gmail.com>
2021-02-28 12:26:53 +0000

    Added Index & Favicon
    Added the index.php main page with a basic skeleton HTML structure.
    
    Also added a favicon.

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Monopoly House Rules</title>
+    <link rel="shortcut icon" type="image/jpg" href="icon/favicon.ico"/>
+
+</head>
+
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
